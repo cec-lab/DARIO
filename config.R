42d9e162f97409064db355e0856c6e12faa89aba
lucacamma <luca.cammarota.lc@gmail.com>
2026-04-20 09:48:04 +0200

    reset config.R con nomi giusti

--- config.R
+++ config.R
@@ -46,11 +46,11 @@ Year = 2023
 
 ## Files ----
 
-rcFileName <- "redcap_test.csv"                                #mettere il dataset di test creato con script: create_dataset_test_algo.R
+rcFileName <- "redcap_test_data.csv"                                #mettere il dataset di test creato con script: create_dataset_test_algo.R
 rcRFileName <- "preprocess_edc.r"
 cedapFileName <- "cedap_plus_2023.csv"
 sdoFileName <- "sdo_stage_11b_clinical_rev_export_final.csv"   
-sdoFileNameMerge <- "sdo_stage12_transcode_export.csv"         #cambiare con quelli veri
+sdoFileNameMerge <- "sdo_stage12_transcode_export.csv"        
 
 ## stage_2 variables list ----
 
